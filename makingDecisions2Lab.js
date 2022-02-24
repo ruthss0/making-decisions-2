c7404f6a040ce668f5aa712bbb58aeb3ebce1a6f
ruthss0 <82294375+ruthss0@users.noreply.github.com>
2022-02-24 11:03:40 -0700

    Update makingDecisions2Lab.js

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -160,7 +160,27 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 
 //Code Here
 
+let letterGrade = 'B';
 
+ switch (letterGrade) {
+  case 'A':
+    console.log("The student is doing excellently.");
+    break;
+  case 'B':
+    console.log( "The student is doing well.");
+    break;
+  case 'C':
+    console.log( "The student is doing alright");
+    break;
+  case 'D':
+    console.log("The student is not doing very well.");
+    break;
+  case 'F':
+    console.log("The student is failing.");
+    break;
+  default:
+    console.log('Not an eligible grade.')
+}
 
 ////////// PROBLEM 11 //////////
 /* The famous FizzBuzz, Devmountain style!
